de7463535f44ab4b72d037082a69ee930787c8b2
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-18 21:34:03 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,4 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class CreateStudents < ActiveRecord::Migration[5.2]
 
     def change
       create_table :students do |t|
@@ -9,4 +9,4 @@ class CreateStudents < ActiveRecord::Migration[5.1]
 
 # Active Record create table method within change method
 # name column with type string
-# rake db:migrate to run the migrations 
+# rake db:migrate to run the migrations
